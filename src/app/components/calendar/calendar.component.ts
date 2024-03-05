123ea8bea99ac3cb2f5690b1c98fcf99d31b51de
Nicolas Hudavert <hudanico@hotmail>
2024-03-05 15:57:59 +0100

    first commit

---
+++ src/app/components/calendar/calendar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cld-calendar',
+  standalone: true,
+  imports: [],
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.scss'
+})
+export class CalendarComponent {
+
+}
