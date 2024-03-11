4d896c604ebd95b174d21e31c0fd9cc2c84f4b32
Nicolas Hudavert <hudanico@hotmail>
2024-03-11 14:30:49 +0100

    add unit tests

--- src/app/components/calendar-header/calendar-header.component.spec.ts
+++ src/app/components/calendar-header/calendar-header.component.spec.ts
@@ -1,23 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DateService } from '../../services/date/date.service';
+import { CalendarComponent } from '../calendar/calendar.component';
 
 import { CalendarHeaderComponent } from './calendar-header.component';
 
 describe('CalendarHeaderComponent', () => {
   let component: CalendarHeaderComponent;
   let fixture: ComponentFixture<CalendarHeaderComponent>;
+  let service: DateService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CalendarHeaderComponent]
-    })
-    .compileComponents();
-    
+      imports: [CalendarHeaderComponent],
+      providers: [DateService],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CalendarHeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = TestBed.inject(DateService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should display proper month and year', () => {
+    fixture.componentRef.setInput('month', 1);
+    fixture.componentRef.setInput('year', 2024);
+    fixture.detectChanges();
+    expect(
+      fixture.debugElement.query(By.css('[data-testid="monthAndYear"]'))
+        .nativeElement.textContent
+    ).toBe('Feb 2024');
   });
 });
--- src/app/services/date/date.service.spec.ts
+++ src/app/services/date/date.service.spec.ts
@@ -13,4 +13,28 @@ describe('DateService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  it('should increment properly the month when month is not last month of year', () => {
+    service.month.set(4);
+    service.incr();
+    expect(service.month()).toEqual(5);
+  });
+  it('should increment properly the month and year when month is the last month of year', () => {
+    service.month.set(11);
+    service.year.set(2024);
+    service.incr();
+    expect(service.month()).toEqual(0);
+    expect(service.year()).toEqual(2025);
+  });
+  it('should decrement properly the month when month is not first month of year', () => {
+    service.month.set(7);
+    service.decr();
+    expect(service.month()).toEqual(6);
+  });
+  it('should decrement properly the month and year when month is the first month of year', () => {
+    service.month.set(0);
+    service.year.set(2012);
+    service.decr();
+    expect(service.month()).toEqual(11);
+    expect(service.year()).toEqual(2011);
+  });
 });
